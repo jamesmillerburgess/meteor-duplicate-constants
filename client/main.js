67c724e1e2d32ebe21a0af6bc5b996e75b84ecd4
James Miller Burgess <jamesmillerburgess@gmail.com>
2018-07-04 13:04:30 +0400

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,4 @@
+const a = 1;
+const a = 2; // <-- Duplicate constants crash the build process
+
+// {{ <-- Malformed javascript like this results in 'Your application has errors. Waiting for file change'
